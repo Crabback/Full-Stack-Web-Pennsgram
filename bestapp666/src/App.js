6631f452ba86e2fd7cde297337539ac0274309fb
Crabback <435705942@qq.com>
2022-10-13 16:00:43 -0400

    added a browse page

--- bestapp666/src/App.js
+++ bestapp666/src/App.js
@@ -2,11 +2,13 @@ import "./Styles.css";
 import React from 'react';
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import BrowsePage from "./pages/BrowsePage";
 //import UserPage from "./pages/User";
 
 export default function App() {
   return (
-    <RegisterPage />
+    <BrowsePage />
   );
 }
 
--- bestapp666/src/components/Header.js
+++ bestapp666/src/components/Header.js
@@ -1,4 +1,7 @@
 import "../Styles.css";
+import React, { Component } from 'react';
+import ReactRoundedImage from "react-rounded-image";
+import MyPhoto from "../images/user_pic1.png";
 
 export function Header(props) {
     if (props.page=="home") {
@@ -16,6 +19,16 @@ export function Header(props) {
                 <div className="header_elements">Home</div>
                 <div className="header_elements">About</div>
                 <div className="header_elements">Contact Us</div>
+                <div className="header_elements"> </div>
+                <ReactRoundedImage
+                image={MyPhoto}
+                roundedColor="#321124"
+                imageWidth="18"
+                imageHeight="18"
+                roundedSize="2"
+                borderRadius="10"
+                />
+                <div className="header_elements">Username</div>
             </div>);
     }
   }
