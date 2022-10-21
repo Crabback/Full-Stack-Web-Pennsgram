26e1d6858f19d3b7ef874198a07b1bae9a0225cb
Crabback <435705942@qq.com>
2022-10-21 17:46:31 -0400

    search fixed

--- bestapp666/client/src/components/SearchBar.js
+++ bestapp666/client/src/components/SearchBar.js
@@ -1,19 +1,19 @@
 import "../Styles.css";
 import React from 'react';
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { selectCurrentUser} from '../pages/UserPage/currentUserSlice'
-import { useSelector } from 'react-redux'
+
 
 export function SearchBar(){
   const navigate = useNavigate();
-  const stateCurrentUser = useSelector(selectCurrentUser);
+  const [input, setInput] = useState('');
 
-  var searchForUsername = stateCurrentUser.username;
   const handleSearch = (e) =>{
     e.preventDefault();
-    navigate('/user/' + searchForUsername);
+    navigate('/user/' + input);
+    setInput('');
   }
     return (
       <Form className="d-flex" onSubmit={handleSearch} style={{paddingTop: "2rem", paddingRight:"20%"}}>
@@ -22,7 +22,8 @@ export function SearchBar(){
           placeholder="Search"
           className="me-2"
           aria-label="Search"
-          onChange={e => searchForUsername = e.target.value }
+          onChange={e => setInput(e.target.value) }
+          value={input}
         />
       <Button type='submit'>Search</Button>
   </Form>
