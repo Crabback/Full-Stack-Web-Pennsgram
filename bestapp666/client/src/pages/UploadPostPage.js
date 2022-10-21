54f8525fbb2dd8a18d8f768c06571fff7c5cba61
dennisgaotang <dennisgaotang@163.com>
2022-10-21 01:44:39 -0400

    adding states for upload

--- bestapp666/client/src/pages/UploadPostPage.js
+++ bestapp666/client/src/pages/UploadPostPage.js
@@ -20,56 +20,56 @@ function UploadPostPage() {
     if (e.target.name === "imageURL") {
       setUrlInput((state) => (e.target.value));
     }}
-    
+
   const handlePostUpload = (e) => {
     e.preventDefault();
   };
-
+    
   function handleChange(e) {
     setMedia(URL.createObjectURL(e.target.files[0]));
   }
 
-    return (
-    <div className="background">
-      <Row>
-      <Col ></Col>
+return (
+<div className="background">
+  <Row>
+  <Col ></Col>
 
-      <Form onSubmit={handlePostUpload}>
-        <Row>
-      <Col >
-          <Form.Group className="mb-3">
+  <Form onSubmit={handlePostUpload}>
+    <Row>
+  <Col >
+      <Form.Group className="mb-3">
               <img src={media} alt="user pic 1" width="400" height="400"></img>
-          </Form.Group>
-          <Form.Group controlId="formFile" className="mb-3" style={{paddingRight: "30%"}}>
-            <Form.Label>Upload Media</Form.Label>
-            <Form.Control type="file" onChange={handleChange}/>
-            <Form.Control name="imageURL" onChange={handleImageURLInput} placeholder="Enter online image url" />
-          </Form.Group>
+      </Form.Group>
+      <Form.Group controlId="formFile" className="mb-3" style={{paddingRight: "30%"}}>
+        <Form.Label>Upload Media</Form.Label>
+        <Form.Control type="file" onChange={handleChange}/>
+        <Form.Control name="imageURL" onChange={handleImageURLInput} placeholder="Enter online image url" />
+      </Form.Group>
 
-      </Col>
-      <Col>
-        <Form.Group className="mb-3" controlId="formBasicDescription">
-          <Form.Label>Description</Form.Label>
+  </Col>
+  <Col>
+    <Form.Group className="mb-3" controlId="formBasicDescription">
+      <Form.Label>Description</Form.Label>
           <Form.Control as="textarea"
-          placeholder="Description about the picture/video." 
-          rows={3}/>
-        </Form.Group>
+      placeholder="Description about the picture/video." 
+      rows={3}/>
+    </Form.Group>
 
        
           <NavLink to={"/user/"+stateCurrentUser.username} className="button_text">
-            <Button variant="primary" type="submit">
-              Upload
-            </Button>
+    <Button variant="primary" type="submit">
+      Upload
+    </Button>
           </NavLink>
 
-      </Col>
-      </Row>
-      </Form>
-      <Col sm={1}></Col>
-      </Row>
-      <div style={{paddingLeft: "2rem", paddingTop: "5rem"}}>
-        <Footer />
-      </div>
-    </div>)
-  }
+  </Col>
+  </Row>
+  </Form>
+  <Col sm={1}></Col>
+  </Row>
+  <div style={{paddingLeft: "2rem", paddingTop: "5rem"}}>
+    <Footer />
+  </div>
+</div>)
+}
 export default UploadPostPage
