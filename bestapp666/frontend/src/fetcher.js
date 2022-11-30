9e6694faeba69c2d49e44c6d42b4d2dd3708e7e8
Crabback <435705942@qq.com>
2022-11-30 01:15:23 -0500

    api completed and tested

--- bestapp666/frontend/src/fetcher.js
+++ bestapp666/frontend/src/fetcher.js
@@ -6,6 +6,51 @@ const getUsers = async () => {
     return res.json()
 }
 
+
+const getUser = async (username) => {
+    var res = await fetch(`localhost:8080/user/${username}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const creatNewUser = async (userObject) => {
+    var res = await fetch(`localhost:8080/user/`, {
+        method: 'POST',
+        body: JSON.stringify(userObject)
+        //body: userObject
+    })
+    return res.json()
+}
+
+const followUser = async (username1, username2) => {
+    var res = await fetch(`localhost:8080/follow`, {
+        method: 'PUT',
+        body: JSON.stringify({"username1": username1, "username2": username2})
+    })
+    return res.json()
+}
+
+const unfollowUser = async (username1, username2) => {
+    var res = await fetch(`localhost:8080/unfollow`, {
+        method: 'PUT',
+        body: JSON.stringify({"username1": username1, "username2": username2})
+    })
+    return res.json()
+}
+
+const getPosts = async () => {
+    var res = await fetch(`localhost:8080/posts`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
 export {
-    getUsers
+    getUsers,
+    getUser,
+    creatNewUser,
+    followUser,
+    unfollowUser,
+    getPosts
 }
